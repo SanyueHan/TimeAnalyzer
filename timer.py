2c0d4eefd51ce13fd469fd261198e3d630d26600
SanyueHan <hansanyue@hotmail.com>
2022-07-15 05:12:30 +0800

    support record calling count

--- timer.py
+++ timer.py
@@ -4,7 +4,8 @@ from time import time
 
 class Timer:
     stack = []
-    recorder = defaultdict(lambda: 0.0)
+    time_recorder = defaultdict(float)
+    count_recorder = defaultdict(int)
 
     def __init__(self, qualname):
         self._qualname = qualname
@@ -12,28 +13,31 @@ class Timer:
 
     def __enter__(self):
         self.stack.append(self._qualname)
+        self.count_recorder[self._qualname] += 1
         self._start = time()
 
     def __exit__(self, exc_type, exc_value, traceback):
         delta = time() - self._start
         self.stack.pop()
 
-        self.recorder[self._qualname] += delta
+        self.time_recorder[self._qualname] += delta
         if self.stack:
             caller_name = self.stack[-1]
-            self.recorder[caller_name] -= delta
+            self.time_recorder[caller_name] -= delta
 
     @classmethod
     def explain_performance_by_name(cls):
-        print(f"Total Time: {sum(cls.recorder.values())}s")
-        for t, n in sorted([(t, n) for n, t in cls.recorder.items()], reverse=True):
-            print(f"{t:10.2f}s    {n}")
+        print(f"Overall Time: {sum(cls.time_recorder.values())}s")
+        data = [("name", "total time(s)", "count")]
+        for n in sorted(cls.time_recorder.keys(), key=cls.time_recorder.get, reverse=True):
+            data.append((n, f"{cls.time_recorder[n]:.2f}", f"{cls.count_recorder[n]}"))
+        cls.__display_table(data, ('<', '>', '>'))
 
     @classmethod
     def explain_performance_by_class(cls):
         print("Divided by Class: ")
         classes = {}
-        for n, t in cls.recorder.items():
+        for n, t in cls.time_recorder.items():
             if '.' in n:
                 class_name, method_name = n.split('.')
                 if class_name in classes:
@@ -47,3 +51,13 @@ class Timer:
             print(f"{cls_}    {classes[cls_].pop('total')}s")
             for t, n in sorted([(t, n) for n, t in classes[cls_].items()], reverse=True):
                 print(f"    {t:10.2f}s    {n}")
+
+    @staticmethod
+    def __display_table(data, fmt, sep='    '):
+        n = len(fmt)
+        assert all(len(row) == n for row in data)
+        max_len = [0] * n
+        for i in range(n):
+            max_len[i] = max(len(row[i]) for row in data)
+        for row in data:
+            print(sep.join(f"{row[i]:{fmt[i]}{max_len[i]}}" for i in range(n)))

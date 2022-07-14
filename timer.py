2198757f2a44226c2681a175815dcc57e5138a0b
SanyueHan <hansanyue@hotmail.com>
2022-07-15 03:19:53 +0800

    mount diagnose methods as Timer's class method

--- timer.py
+++ timer.py
@@ -22,3 +22,28 @@ class Timer:
         if self.stack:
             caller_name = self.stack[-1]
             self.recorder[caller_name] -= delta
+
+    @classmethod
+    def explain_performance_by_name(cls):
+        print(f"Total Time: {sum(cls.recorder.values())}")
+        for t, n in sorted([(t, n) for n, t in cls.recorder.items()], reverse=True):
+            print(f"{t:10.2f}ms    {n}")
+
+    @classmethod
+    def explain_performance_by_class(cls):
+        print("Divided by Class: ")
+        classes = {}
+        for n, t in cls.recorder.items():
+            if '.' in n:
+                class_name, method_name = n.split('.')
+                if class_name in classes:
+                    classes[class_name][method_name] = t
+                else:
+                    classes[class_name] = {method_name: t}
+            # todo: normal functions
+        for cls_method_dict in classes.values():
+            cls_method_dict['total'] = sum(cls_method_dict.values())
+        for cls_ in sorted(classes.keys(), key=lambda i: classes[i]['total'], reverse=True):
+            print(f"{cls_}    {classes[cls_].pop('total')}ms")
+            for t, n in sorted([(t, n) for n, t in classes[cls_].items()], reverse=True):
+                print(f"    {t:10.2f}ms    {n}")

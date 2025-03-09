cf578f131c6a996850d59f95e96f00872166dae5
hansanyue <hansanyue@hotmail.com>
2025-03-09 17:05:54 +0800

    release the tool as a wheel

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+setuptools.setup(
+    name="time-analyzer",
+    version="1.0.0",
+    author="Han Sanyue",
+    author_email="hansanyue@hotmail.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
